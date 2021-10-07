d939a04ee7ea94c2d8f94cf5e81dd67e981a2ed3
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2021-10-07 21:37:27 +0200

    added nuget packages.

--- JPV4PC_HFT_2021221/JPV4PC_HFT_2021221 Data/Class1.cs
+++ JPV4PC_HFT_2021221/JPV4PC_HFT_2021221 Data/Class1.cs
@@ -2,6 +2,7 @@
 
 namespace JPV4PC_HFT_2021221_Data
 {
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="|DataDirectory|\Database1.mdf"; Integrated Security=True; MultipleActiveResultSets=True
     public class Class1
     {
     }
