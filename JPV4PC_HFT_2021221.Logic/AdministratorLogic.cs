cca2cb2a1fbcefdff53f34328d092139842532ea
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2021-11-16 23:16:15 +0100

    created the AdministratorLogic interface.

---
+++ JPV4PC_HFT_2021221.Logic/AdministratorLogic.cs
@@ -0,0 +1,14 @@
+﻿using JPV4PC_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPV4PC_HFT_2021221.Logic
+{
+    public class AdministratorLogic
+    {
+
+    }
+}
---
+++ JPV4PC_HFT_2021221.Logic/IAdministratorLogic.cs
@@ -0,0 +1,23 @@
+﻿using JPV4PC_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPV4PC_HFT_2021221.Logic
+{
+    public interface IAdministratorLogic
+    {
+        Reservations GetReservation(int id);
+        IEnumerable<Reservations> GetAllReservations();
+        Fans GetFan(int id);
+        IEnumerable<Fans> GetAllFans();
+        void UpdateServiceCost(int serviceId, int cost);
+        void UpdateArtistCost(int artistId, int cost);
+        //////// non-crud ops should be added
+
+
+
+    }
+}
