280ff0397de4ac0c250086ee36cb24bddf28b93e
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2021-11-19 19:25:39 +0100

    fixed some problems about non-crud funcs.

--- JPV4PC_HFT_2021221.Logic/IAdministratorLogic.cs
+++ JPV4PC_HFT_2021221.Logic/IAdministratorLogic.cs
@@ -20,12 +20,14 @@ namespace JPV4PC_HFT_2021221.Logic
         void UpdateServiceCost(int serviceId, int cost);
         void UpdateArtistCost(int artistId, int cost);
         //////// non-crud ops should be added
-        IEnumerable<ArtistEarnings> ArtistEarnings();
-        IEnumerable<ArtistEarnings> MostPaidArtist();
-        IEnumerable<ArtistEarnings> LessPaidArtist();
+        
 
-        IEnumerable<FanRating> BestFan();
-        IEnumerable<FanRating> WorstFan();
+
+        IEnumerable<KeyValuePair<string,int>> ArtistEarnings();
+        IEnumerable<KeyValuePair<string,int>> MostPaidArtist();
+        IEnumerable<KeyValuePair<string, int>> LessPaidArtist();
+        IEnumerable<KeyValuePair<string, int>> BestFan();
+        IEnumerable<KeyValuePair<string, int>> WorstFan();
 
         
 
