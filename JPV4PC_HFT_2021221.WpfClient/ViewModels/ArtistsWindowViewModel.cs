3a7d4168c3266bb8fb89630a9ac1dd3f66fe6266
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2022-04-02 13:44:54 +0200

    Done with artists window.

---
+++ JPV4PC_HFT_2021221.WpfClient/ViewModels/ArtistsWindowViewModel.cs
@@ -0,0 +1,99 @@
+﻿using JPV4PC_HFT_2021221.Models;
+using JPV4PC_HFT_2021221.WpfClient.Clients;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JPV4PC_HFT_2021221.WpfClient.ViewModels
+{
+    class ArtistsWindowViewModel : ObservableRecipient
+    {
+        private ApiClient _apiClient = new ApiClient();
+
+        public ObservableCollection<Artists> Artists { get; set; }
+        private Artists _selectedArtist;
+
+        public Artists SelectedArtist
+        {
+            get => _selectedArtist;
+            set
+            {
+                SetProperty(ref _selectedArtist, value);
+            }
+        }
+        private int _selectedArtistIndex;
+
+        public int SelectedArtistIndex
+        {
+            get => _selectedArtistIndex;
+            set
+            {
+                SetProperty(ref _selectedArtistIndex, value);
+            }
+        }
+        public RelayCommand AddArtistCommand { get; set; }
+        public RelayCommand EditArtistCommand { get; set; }
+        public RelayCommand DeleteArtistCommand { get; set; }
+        public ArtistsWindowViewModel()
+        {
+            Artists = new ObservableCollection<Artists>();
+
+            _apiClient
+                .GetAsync<List<Artists>>("http://localhost:37793/artists")
+                .ContinueWith((artists) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        artists.Result.ForEach((artist) =>
+                        {
+                            Artists.Add(artist);
+                        });
+                    });
+                });
+
+            AddArtistCommand = new RelayCommand(AddArtist);
+            EditArtistCommand = new RelayCommand(EditArtist);
+            DeleteArtistCommand = new RelayCommand(DeleteArtist);
+            
+        }
+        private void AddArtist()
+        {
+
+        }
+        private void EditArtist()
+        {
+            _apiClient
+                .PutAsync(SelectedArtist, "http://localhost:37793/artists")
+                .ContinueWith((task) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        Artists.Remove(SelectedArtist);
+                        Artists.Insert(SelectedArtistIndex, SelectedArtist);
+                    });
+                });
+        }
+        private void DeleteArtist()
+        {
+            _apiClient
+                .DeleteAsync(SelectedArtist.Id, "http://localhost:37793/artists")
+                .ContinueWith((task) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        Artists.Remove(SelectedArtist);
+                    });
+                });
+        }
+
+
+
+
+    }
+}
---
+++ JPV4PC_HFT_2021221.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPV4PC_HFT_2021221.WpfClient.ViewModels
+{
+    public class MainWindowViewModel :ObservableRecipient
+    {
+        public RelayCommand ManageArtistsCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ManageArtistsCommand = new RelayCommand(OpenArtistsWindow);
+        }
+
+        private void OpenArtistsWindow()
+        {
+            new ArtistsWindow().Show();
+        }
+
+    }
+}
