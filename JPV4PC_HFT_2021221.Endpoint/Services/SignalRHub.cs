1f1b74fb9c36ee48297599a0689b94c030514bfa
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2022-04-29 15:56:14 +0200

    SignalR and JSclient.
    added SignalR to the endpoints and created JsClient with some html and js and css files.

---
+++ JPV4PC_HFT_2021221.Endpoint/Services/SignalRHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JPV4PC_HFT_2021221.Endpoint.Services
+{
+    public class SignalRHub
+    {
+    }
+}
